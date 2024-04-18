1f550b22142a0aaa9bcd2eb9355ae162bbee73db
MattUehling <uehlingmatt@gmail.com>
2024-04-18 18:44:46 -0500

    Update television.py

--- television.py
+++ television.py
@@ -1,58 +1,91 @@
 class Television:
-    MIN_VOLUME =0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
-    def __init__(self, status = False, muted = False, volume = MIN_VOLUME, channel = MIN_CHANNEL):
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
+
+    def __init__(self, status: bool = False, muted: bool = False, volume: int = MIN_VOLUME, channel: int = MIN_CHANNEL) -> None:
+        """
+        Initializes the Television object.
+        """
         self.status = status
         self.muted = muted
         self.volume = volume
         self.channel = channel
         self.savedVolume = volume
-    def power(self):
-        if self.status == False:
+
+    def power(self) -> bool:
+        """
+        Turns the television on and off.
+        """
+        if not self.status:
             self.status = True
         else:
             self.status = False
-    def mute(self):
-        if self.status == True:
-            if self.muted == False:
+        return self.status
+
+    def mute(self) -> bool:
+        """
+        Unmutes and mutes the television.
+        """
+        if self.status:
+            if not self.muted:
                 self.muted = True
                 self.savedVolume = self.volume
                 self.volume = 0
             else:
                 self.muted = False
-            
-    def channel_up(self):
-        if self.status == True:
+                self.volume = self.savedVolume
+        return self.muted
+
+    def channel_up(self) -> int:
+        """
+        Increases the channel number by one.
+        """
+        if self.status:
             if self.channel == self.MAX_CHANNEL:
                 self.channel = self.MIN_CHANNEL
             else:
-                self.channel+=1
+                self.channel += 1
+        return self.channel
 
-    def channel_down(self):
-        if self.status == True:
+    def channel_down(self) -> int:
+        """
+        Decreases the channel number by one.
+        """
+        if self.status:
             if self.channel == self.MIN_CHANNEL:
                 self.channel = self.MAX_CHANNEL
             else:
-                self.channel-=1
+                self.channel -= 1
+        return self.channel
 
-    def volume_up(self):
-        if self.status == True:
-            if self.muted == True:
+    def volume_up(self) -> int:
+        """
+        Increase the volume level by one.
+        """
+        if self.status:
+            if self.muted:
                 self.muted = False
                 self.volume = self.savedVolume
             if self.volume != self.MAX_VOLUME:
-                self.volume+=1
+                self.volume += 1
+        return self.volume
 
-    def volume_down(self):
-        if self.status == True:
-            if self.muted == True:
+    def volume_down(self) -> int:
+        """
+        Decrease the volume level by one.
+        """
+        if self.status:
+            if self.muted:
                 self.muted = False
                 self.volume = self.savedVolume
             if self.volume != self.MIN_VOLUME:
-                self.volume-=1
-    def __str__(self):
-        return  f'Power = {self.status}, Channel = {self.channel}, Volume {self.volume}'
+                self.volume -= 1
+        return self.volume
 
-            
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the Television object.
+        """
+        return f'Power = {self.status}, Channel = {self.channel}, Volume = {self.volume}'
